d43b10ff0abbf085e704bc5129c7734b04d32ff3
Tenuun <tenuuni.08@gmail.com>
2024-03-02 21:28:36 +0800

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+echo you NEED A USB FOR THIS TO WORK!!!!!!! to run this script you must add the argument "ihaveausbthatislargerthan8gb" to the script (e.g. bash install.sh ihaveausbthatislargerthab8gb)
+if [ $1 == "ihaveausbthatislargerthan8gb" ]
+then
+curl https://drive.massgrave.dev/en-us_windows_10_consumer_editions_version_22h2_updated_feb_2024_x64_dvd_c937d96c.iso -O ~/Desktop/temp.iso
+curl https://github.com/balena-io/etcher/releases/download/v1.18.11/balenaEtcher-1.18.11-x64.AppImage -O ~/Desktop/temp2.AppImage
+~/Desktop/temp2.AppImage
+echo now you must do this yourself, click Flash from File and select the temp.iso file that has been downloaded to your desktop.
+echo click Select target, choose your !!USB THAT IS LARGER THAN 8GB!! and click flash
+echo do not unplug the usb or you will fuck it up
+echo then just reboot into the usb, if youre a big noob and dont know how to do so, gtfo :rofl:
+echo install and voila
+echo it works !!!!!!!
+else
+echo you havent added ihaveausbthatislargerthan8gb to the script. read the statement above and add the required argument if you can.
+fi
+exit
